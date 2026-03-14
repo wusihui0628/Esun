57c895f67c601ac1dc30dd60bae08e79e18d6c39
wusihui <wusihui303@gmail.com>
2026-03-15 00:27:10 +0800

    command some unused line

--- backend/vote/src/main/java/com/demo/vote/presentation/controller/VoteItemController.java
+++ backend/vote/src/main/java/com/demo/vote/presentation/controller/VoteItemController.java
@@ -3,7 +3,7 @@ package com.demo.vote.presentation.controller;
 import com.demo.vote.service.VoteItemService;
 import com.demo.vote.presentation.response.VoteItemResponse;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
